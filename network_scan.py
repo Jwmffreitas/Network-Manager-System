d7f1c10b20d1865743d9bd933fb7e5e209934c94
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-06-16 00:40:14 -0300

    Scan devices on local network is working

---
+++ network_scan.py
@@ -0,0 +1,53 @@
+import scapy.all as scapy
+import argparse
+import requests
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target', help='Target IP Address/Adresses (Ex. 192.168.1.1/24)')
+    options = parser.parse_args()
+
+    # Verifica se o usuário não especificou o endereço IP
+    # Saia do programa se estiver faltando argumento
+    # Enquanto sai, exiba a mensagem de erro
+    if not options.target:
+        # Execute caso a interface não tenha sido especificada
+        parser.error("[-] Por favor especifique um Endereço IP ou Endereço de Rede, use --help para mais informações.")
+    return options
+
+
+def scan(ip):
+    arp_req_frame = scapy.ARP(pdst=ip)
+
+    broadcast_ether_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
+
+    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout=1, verbose=False)[0]
+    result = []
+    for i in range(0, len(answered_list)):
+        ip_address = answered_list[i][1].psrc
+        mac_address = answered_list[i][1].hwsrc
+        short_mac = mac_address.replace(":", "")[:-2]
+        device = requests.get(f'https://macvendors.com/query/{short_mac}').text
+
+        if "<" in device:
+            device = "Erro ao consultar detalhes"
+
+        client_dict = {"ip": ip_address, "mac": mac_address, "device": device}
+        result.append(client_dict)
+
+    return result
+
+
+def display_result(result):
+    print("---------------------------------------------------\nIP Address\tMAC Address\tDevice "
+          "Details\n---------------------------------------------------")
+    for i in result:
+        print("{}\t{}\t{}".format(i["ip"], i["mac"], i["device"]))
+
+
+options = get_args()
+scanned_output = scan(options.target)
+display_result(scanned_output)
