16220d441ec06064c0c13a55433d196bee793ee5
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-06-17 21:48:52 -0300

    Styling first screen

--- interface/main.js
+++ interface/main.js
@@ -2,6 +2,14 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+const electron = require('electron')
+
+// Enable live reload for Electron too
+require('electron-reload')(__dirname, {
+    // Note that the path to electron may vary according to the main file
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
