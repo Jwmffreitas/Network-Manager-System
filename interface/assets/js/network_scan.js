e612c4de84e67a06f2de32c84081ec7ea26ebc8a
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-06-30 19:32:08 -0300

    Improving UX

--- interface/assets/js/network_scan.js
+++ interface/assets/js/network_scan.js
@@ -1,12 +1,15 @@
 let {PythonShell} = require('python-shell')
-var path = require("path")
+let path = require("path")
+
+let wasSearched = false
 
 
 function network_scan() {
-
-  //var city = document.getElementById("city").value
-
-  console.log(__dirname.replace(/\\/g, '/'))
+  if(wasSearched) {
+    document.querySelector('#network_table').style.display = "none"
+  }
+  document.querySelector('#loading').style.display = "inline-block"
+  document.querySelector('#local_network').style.display = "none"
 
   var options = {
     scriptPath : path.join(__dirname, '/../')
@@ -18,19 +21,22 @@ function network_scan() {
     if (err) throw err;
     // result is an array consisting of messages collected
     //during execution of script.
-    //console.log('result: ', result.toString());
     show_network_data(result)
   });
 }
 
 const show_network_data = (data) => {
     document.querySelector('#network_table').style.display = "table"
-    document.querySelector('#local_network').style.display = "none"
+    document.querySelector('#loading').style.display = "none"
 
     data = data[0]
     data = data.replace(/'/g,'"')
     data = JSON.parse(String(data))
     console.log(JSON.stringify(data))
+
+    if(wasSearched) {
+      document.querySelector('#network_table').innerHTML = "" 
+    }
 
     data.map(endpoint => {
         console.log(JSON.stringify(endpoint))
@@ -41,6 +47,8 @@ const show_network_data = (data) => {
             <td>${endpoint.device}</td>
         </tr>`
     });
+
+    wasSearched = true
 }
 
 module.exports = {
