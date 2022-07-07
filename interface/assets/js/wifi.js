fcbb309689330a1a539c5f3083d287c77298ae0d
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-07-06 21:14:12 -0300

    Get wifi passwords is working

--- interface/assets/js/wifi.js
+++ interface/assets/js/wifi.js
@@ -1,4 +1,5 @@
 let {PythonShell} = require('python-shell')
+let path = require("path")
 let wasScanned = false
 
 const show_password = (button, password) => {
@@ -11,4 +12,55 @@ const show_password = (button, password) => {
         document.querySelector(`#${button}`).classList.remove('fa-eye');
         document.querySelector(`#${button}`).classList.add('fa-eye-slash');
     }
+}
+
+const show_wifi_data = (data) => {
+    document.querySelector('#network_table').style.display = "table"
+    document.querySelector('#loading').style.display = "none"
+
+    data = data[0]
+    data = data.replace(/'/g,'"')
+    data = JSON.parse(String(data))
+
+    let profiles = data[0]
+    let passwords = data[1]
+
+    console.log(JSON.stringify(profiles))
+    console.log(JSON.stringify(passwords))
+
+    if(wasScanned) {
+      document.querySelector('#network_table').innerHTML = "" 
+    }
+
+    for(let i = 0; i < profiles.length; i++) {
+      document.querySelector('#network_table').innerHTML += `
+      <tr>
+          <td>${profiles[i]}</td>
+          <td id="password-${i}">${passwords[i]}</td>
+          <td><i class="fa-solid fa-eye eye" id="see-${i}" onclick="show_password('see-${i}', 'password-${i}')"></i></td>
+      </tr>`
+    }
+
+    wasScanned = true
+}
+
+const scan_wifi = () => {
+    if(wasScanned) {
+      document.querySelector('#network_table').style.display = "none"
+    }
+    document.querySelector('#loading').style.display = "inline-block"
+    document.querySelector('#broadcast').style.display = "none"
+  
+    var options = {
+      scriptPath : path.join(__dirname, '/../')
+    }
+  
+    console.log(options)
+    
+    PythonShell.run('wifi_password.py', options, function (err, result){
+      if (err) throw err;
+      // result is an array consisting of messages collected
+      //during execution of script.
+      show_wifi_data(result)
+    });
 }
